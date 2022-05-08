58e05d3139b038f1aae1a9d7394d672fdb3a887f
ReddNev <creedlll@mail.ru>
2022-05-08 12:33:36 +0300

    Fix BUG

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 import logging
 from aiogram import executor
-from create_bot import dp
+from create_bot import dp, bot
 from handlers import user
+from aiogram.types import BotCommand
 
 logging.basicConfig(level=logging.INFO)
+
+
+async def set_commands(bot: bot):
+    commands = [
+        BotCommand(command="/start", description="Запуск бота и регистрация новых пользователей"),
+        BotCommand(command="/wallets", description="Ваш кошелек"),
+
+    ]
+    await bot.set_my_commands(commands)
+
 
 """ Function launch """
 user.register_handlers_client(dp)
--- handlers/user.py
+++ handlers/user.py
@@ -1,15 +1,16 @@
 import typing
 
 from aiogram import types, Dispatcher
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardRemove
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+
+from create_bot import bot
 from database.database import get_user_by_id, insert_new_user, get_addres_by_id
-from create_bot import dp, bot
 
 from config import WALLETS, WALLETS_COMMAND
 from services.client import Client
 
 
-# @dp.message_handler(commands=['start'])
 async def start(message: types.Message):
     ''' Response to command start '''
     chat_id = message.from_user.id
@@ -22,13 +23,13 @@ async def start(message: types.Message):
         await message.answer("User added to the system")
 
 
-# @dp.message_handler(commands=['wallets'])
-async def get_wallets(message: types.Message):
+async def get_wallets(callback_query: types.CallbackQuery):
     """
     Возвращает кошельки
-    :param message:
+    :param :
     :return:
     """
+
     group_btn_s = InlineKeyboardMarkup(row_width=2)
     btn_s = []
     for wallet_key in WALLETS.keys():
@@ -38,28 +39,29 @@ async def get_wallets(message: types.Message):
         # callback_data TYPE: wallet_tron
     # back = InlineKeyboardButton(f"<= BACK")
     group_btn_s.add(*btn_s)
-    await message.answer('Ваши кошельки', reply_markup=group_btn_s)
+    await bot.answer_callback_query(callback_query.id)
+    await callback_query.message.edit_text('Ваши кошельки', reply_markup=group_btn_s)
 
 
-# @dp.callback_query_handler(lambda x: x.data[:7] == 'wallet_' and x.data[7:] in WALLETS)
 async def get_wallet_commands(callback_query: types.CallbackQuery):
-    group_btn_s = InlineKeyboardMarkup(row_width=2)
-    btn_s = []
-    for method_key, method_value in WALLETS_COMMAND.items():
-        btn_s.append(
-            InlineKeyboardButton(f"{method_value}", callback_data=f"type_{method_key.lower()}_{callback_query.data[7:]}")
-        )
-        # callback_query.data[7:] TYPE: NETWORK --- TRON, SOLANA и тд
-        # method_key TYPE: METHOD --- BALANCE, SEND и тд
-        # callback_data TYPE: type_<METHOD>_<NETWORK>
+        group_btn_s = InlineKeyboardMarkup(row_width=2)
+        btn_s = []
+        for method_key, method_value in WALLETS_COMMAND.items():
+           btn_s.append(
+                InlineKeyboardButton(f"{method_value}", callback_data=f"type_{method_key.lower()}_{callback_query.data[7:]}"
+                                     )
+            )
+           # btn_main = InlineKeyboardButton(text='Главное меню', callback_data='get_wallets')
+           # callback_query.data[7:] TYPE: NETWORK --- TRON, SOLANA и тд
+            # method_key TYPE: METHOD --- BALANCE, SEND и тд
+            # callback_data TYPE: type_<METHOD>_<NETWORK>
+        back = InlineKeyboardButton('back', callback_data='wallets')
+        btn_s.append(back)
+        group_btn_s.add(*btn_s)
+        await bot.answer_callback_query(callback_query.id)
+        await callback_query.message.edit_text(text='Что Вы хотите:', reply_markup=group_btn_s)
 
-    back = InlineKeyboardButton(text="Назад", callback_data="Wallets")
-    group_btn_s.add(*btn_s, back)
-    await callback_query.message.answer('Что Вы хотите:', reply_markup=group_btn_s)
-    await bot.delete_message(chat_id=callback_query.from_user.id, message_id=callback_query.message.message_id)
 
-
-# @dp.callback_query_handler(lambda x: x.data[:13] == 'type_balance_' and x.data[13:] in WALLETS)
 async def get_tokens(callback_query: types.CallbackQuery):
     group_btn_s = InlineKeyboardMarkup(row_width=2)
     btn_s = []
@@ -71,13 +73,13 @@ async def get_tokens(callback_query: types.CallbackQuery):
     # callback_query.data[13:] TYPE: NETWORK --- TRON, SOLANA и тд
     # wallet_token TYPE: TOKEN --- TRX, USDT и тд
     # callback_data TYPE: balance_<NETWORK>_<TOKEN>
-    back = InlineKeyboardButton(text="Назад", callback_data="Wallets")
-    group_btn_s.add(*btn_s, back)
-    await callback_query.message.answer('Какой ТОКЕН:', reply_markup=group_btn_s)
-    await bot.delete_message(chat_id=callback_query.from_user.id, message_id=callback_query.message.message_id)
+    back = InlineKeyboardButton('back', callback_data='wallets')
+    btn_s.append(back)
+    group_btn_s.add(*btn_s)
+    await bot.answer_callback_query(callback_query.id)
+    await callback_query.message.edit_text(text='Какой ТОКЕН:', reply_markup=group_btn_s)
 
 
-# @dp.callback_query_handler(lambda x: x.data[:8] == "balance_")
 async def get_balance(callback_query: types.CallbackQuery):
 
     _, network, token = is_valid(data=callback_query.data)
@@ -95,7 +97,8 @@ async def get_balance(callback_query: types.CallbackQuery):
 
 def register_handlers_client(dp: Dispatcher):
     dp.register_message_handler(start, commands=['start'])
-    dp.register_message_handler(get_wallets, commands=['wallets'])
+    # dp.register_message_handler(get_wallets, commands=['wallets'])
+    dp.register_callback_query_handler(get_wallets, lambda x: x.data == 'wallets')
     dp.register_callback_query_handler(get_wallet_commands, lambda x: x.data[:7] == 'wallet_' and x.data[7:] in WALLETS)
     dp.register_callback_query_handler(get_tokens, lambda x: x.data[:13] == 'type_balance_' and x.data[13:] in WALLETS)
     dp.register_callback_query_handler(get_balance, lambda x: x.data[:8] == "balance_")
